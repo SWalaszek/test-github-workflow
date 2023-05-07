b4867480ac30fcc3a8cce79f11058fe65d2809fe
SWalaszek <105596557+SWalaszek@users.noreply.github.com>
2023-05-07 16:03:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+"""
+WSB Flask app.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """
+    Zwraca wiadomosc powitalna
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
